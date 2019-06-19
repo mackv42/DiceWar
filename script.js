4cd2b8e269efaba16bed6e1e2a787ebddf8de744
Vincent Mackay <mackv42@gmail.com>
2019-06-19 12:39:44 -0700

    Players take turns and player wins after three winning matches

--- script.js
+++ script.js
@@ -31,15 +31,69 @@ function createDice(div){
 	return new Array(new Die(4, div), new Die(6, div), new Die(8, div), new Die(10, div), new Die(12, div), new Die(20, div));
 }
 
-let p1Div = document.getElementById("p1");
-let p2Div = document.getElementById("p2");
-let player1 = {arr: createDice(p1Div)};
-let player2 = {arr: createDice(p2Div)};
+let p1Div = document.getElementById("p1").getElementsByClassName('dice')[0];
+let p2Div = document.getElementById("p2").getElementsByClassName('dice')[0];
+
+let player1 = {
+	arr: createDice(p1Div),
+	score: 0,
+	wins: 0,
+	isTurn: true
+};
+
+let player2 = {
+	arr: createDice(p2Div),
+	score: 0,
+	wins: 0,
+	isTurn: false
+};
+
 
 document.getElementById('p1Roll').onclick = function(){
-	document.getElementById("p1Score").innerHTML = getScore(player1.arr);
+	if(!player1.isTurn)
+		return;
+	player1.score = getScore(player1.arr);
+	document.getElementById("p1Score").innerHTML = player1.score;
+	player1.isTurn = false;
+	player2.isTurn = true;
+	p1Div.style.border = "none";
+	p2Div.style.border = "thick solid #0000FF"; 
+}
+
+
+function resetValues(){
+	player1.wins = 0;
+	player2.wins = 0;
+	document.getElementsByClassName('Wins')[0].innerHTML = "";
+	document.getElementsByClassName('Wins')[1].innerHTML = "";
 }
 
 document.getElementById('p2Roll').onclick = function(){
+	if(!player2.isTurn)
+		return;
+
+	player2.score = getScore(player2.arr);
 	document.getElementById("p2Score").innerHTML = getScore(player2.arr);
+	player1.isTurn = true;
+	player2.isTurn = false;
+
+	p2Div.style.border = "none";
+	p1Div.style.border = "thick solid #0000FF"; 
+
+	if(player1.score > player2.score){
+		player1.wins++;
+		document.getElementsByClassName("Wins")[0].innerHTML = player1.wins;
+	} else{
+		player2.wins++;
+		document.getElementsByClassName("Wins")[1].innerHTML = player2.wins;
+	}
+
+	//Checks if there is a winner
+	if(player1.wins > 2){
+		alert("Player 1 Wins!");
+		resetValues();
+	} else if(player2.wins > 2){
+		alert("Player 2 Wins!");
+		resetValues();
+	}
 }
